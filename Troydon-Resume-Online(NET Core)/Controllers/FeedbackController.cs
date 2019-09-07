0e668ce6b28e0e91fd2ce168cd8b86e6e943e0fb
Troydon Luicien <tluicien@yahoo.co.nz>
2019-09-07 15:44:22 +1000

    updated feedback controller to return just what is typed into the url

---
+++ Troydon-Resume-Online(NET Core)/Controllers/FeedbackController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Troydon_Resume_Online_NET_Core_.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        //GET: /<controllers>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //POST
+        public IActionResult Comment(string id)
+        {
+            return new ContentResult { Content = id };
+        }
+    }
+}

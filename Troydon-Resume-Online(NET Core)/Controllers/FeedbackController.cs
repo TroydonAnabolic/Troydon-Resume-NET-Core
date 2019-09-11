51e60ef160fba74eeca21d5feb972eb1ccf76deb
Troydon Luicien <tluicien@yahoo.co.nz>
2019-09-12 09:17:43 +1000

    seed data returned with new view for comments

--- Troydon-Resume-Online(NET Core)/Controllers/FeedbackController.cs
+++ Troydon-Resume-Online(NET Core)/Controllers/FeedbackController.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Troydon_Resume_Online_NET_Core_.Models;
 
 namespace Troydon_Resume_Online_NET_Core_.Controllers
 {
-    // Class Starts with localhost:XXXX/feedback
+    // Class Starts with localhost:44348/feedback
     [Route("feedback")]
     public class FeedbackController : Controller
     {
@@ -17,16 +18,20 @@ namespace Troydon_Resume_Online_NET_Core_.Controllers
             return new ContentResult { Content = "My CV Site feedback" };
         }
 
-        // TODO: Remove test values
+        // Route: localhost:44348/feedback/2019/September/11/test
         [Route("{year:min(2019)}/{month?}/{day:range(1,31)?}/{key?}")]
         //POST
         public IActionResult Comment(int year, string month, int day, string key)
         {
-            return new ContentResult
+            var comment = new Comment
             {
-                Content = string.Format("Year: {0} Month: {1} Day: {2} Key: {3}",
-                                        year, month, day, key )
+                Title = "My Feedback",
+                Commented = DateTime.Now,
+                Person = "Troydon",
+                Body = "This is a great website, right?",
             };
+
+            return View(comment);
         }
     }
 }

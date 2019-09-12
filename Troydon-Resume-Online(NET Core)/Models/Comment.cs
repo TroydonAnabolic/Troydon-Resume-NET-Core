8252ba5bc90fb733b8e76d974529ca79deb55045
Troydon Luicien <tluicien@yahoo.co.nz>
2019-09-13 09:05:18 +1000

    added more form data

--- Troydon-Resume-Online(NET Core)/Models/Comment.cs
+++ Troydon-Resume-Online(NET Core)/Models/Comment.cs
@@ -10,6 +10,8 @@ namespace Troydon_Resume_Online_NET_Core_.Models
         public string Title { get; set; }
         public string Person { get; set; }
         public string Body { get; set; }
+        public string Position { get; set; }
+        public string Rate { get; set;  }
         public DateTime Commented { get; set; }
 
     }

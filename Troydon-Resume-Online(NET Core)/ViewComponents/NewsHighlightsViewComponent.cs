f4022df9dea6cfc7e099f4239e3b213aa9a2af9c
Troydon Luicien <tluicien@yahoo.co.nz>
2019-09-17 21:33:39 +1000

    added model to enhance view component and made seed data

--- Troydon-Resume-Online(NET Core)/ViewComponents/NewsHighlightsViewComponent.cs
+++ Troydon-Resume-Online(NET Core)/ViewComponents/NewsHighlightsViewComponent.cs
@@ -3,15 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Troydon_Resume_Online_NET_Core_.Models;
 
 namespace Troydon_Resume_Online_NET_Core_.ViewComponents
 {
     [ViewComponent]
-    public class NewsHighlightsViewComponent
+    public class NewsHighlightsViewComponent : ViewComponent
     {
-        public string Invoke()
+        private readonly FeedbackDataContext _db;
+        public NewsHighlightsViewComponent(FeedbackDataContext db)
         {
-            return "TODO: Show recent news highlights";
+           this._db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var highlights = _db.NewsHighlights.ToArray();
+
+            return View(highlights);
         }
     }
 }

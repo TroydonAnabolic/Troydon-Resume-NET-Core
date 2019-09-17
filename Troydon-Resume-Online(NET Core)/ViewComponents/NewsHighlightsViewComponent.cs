912c3cfbc99e28f26c15d4a2e40ea9d737be779d
Troydon Luicien <tluicien@yahoo.co.nz>
2019-09-17 21:01:32 +1000

    added view component class to return view components which contain data

---
+++ Troydon-Resume-Online(NET Core)/ViewComponents/NewsHighlightsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Troydon_Resume_Online_NET_Core_.ViewComponents
+{
+    [ViewComponent]
+    public class NewsHighlightsViewComponent
+    {
+        public string Invoke()
+        {
+            return "TODO: Show recent news highlights";
+        }
+    }
+}
